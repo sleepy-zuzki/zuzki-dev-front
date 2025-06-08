7a7db1feabd9d74197562c91b6e2127467475b45
Zuzki <sleepy@zuzki.dev>
2025-06-08 16:01:14 -0500

    ### Add environment interface and configuration for GitHub and Make.com APIs
    Introduced a strongly-typed `Environment` interface and updated the environment files with new API base URLs for GitHub and Make.com. Enhanced the `angular.json` configuration to include a specific file replacement for the development environment.
    
    ---
    
    The newly added `Environment` interface ensures type safety for environment configurations. The environment files now distinguish between production and development settings, with proper API base URLs. The build process for development mode has been refined with `fileReplacements` in `angular.json`.
    
    ---
    
    #### Changes:
    
    - **environment.development**
      - Added `production`, `githubApiUrl`, and `makeApiUrl` properties.
      - Implemented type safety using the new `Environment` interface.
    
    - **environment**
      - Aligned with `Environment` interface.
      - Updated properties to include `production`, `githubApiUrl`, and `makeApiUrl`.
    
    - **environment.interface**
      - Created a new interface to define properties for environment configuration.
    
    - **angular**
      - Configured development builds to replace the default environment file with the new `environment.development` configuration.
    
    Signed-off-by: Zuzki <sleepy@zuzki.dev>

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  'GITHUB_DATA_URL': ''
+import { Environment } from '@core/interfaces/environment.interface';
+
+export const environment: Environment = {
+  production: false,
+  githubApiUrl: 'https://github-worker.zuzki.dev',
+  makeApiUrl: 'https://hook.us2.make.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,7 @@
-export const environment = {
-  'GITHUB_DATA_URL': '',
+import { Environment } from '@core/interfaces/environment.interface';
+
+export const environment: Environment = {
+  production: true,
   'githubApiUrl': 'https://github-worker.zuzki.dev',
   'makeApiUrl': 'https://hook.us2.make.com'
 };
