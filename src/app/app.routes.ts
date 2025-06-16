6fca29b30ab02b12f2ad4d3a32e4ab41cf676844
Zuzki <sleepy@zuzki.dev>
2025-06-16 04:03:34 -0500

    feat: add page titles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,17 +2,22 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'about',
+    title: 'Acerca de Sleepy Zuzki - Desarrollo Web y Streaming',
     loadComponent: () => import('@pages/about.page').then(m => m.AboutPage)
   },
   {
     path: 'works',
+    title: 'Proyectos de Sleepy Zuzki - Soluciones Interactivas',
     loadComponent: () => import('@pages/works.page').then(m => m.WorksPage)
   },
   {
     path: 'works/:id',
+    title: 'Detalle de Proyecto - Sleepy Zuzki',
     loadComponent: () => import('@pages/work-details.page').then(m => m.WorkDetailsPage)
   },
-  { path: '',
+  {
+    path: '',
+    title: 'Portafolio de Sleepy Zuzki - Inicio',
     loadComponent: () => import('@pages/home.page').then(m => m.HomePage)
   }
 ];
